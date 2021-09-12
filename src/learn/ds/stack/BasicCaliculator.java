c206a7f4626d68c55fd432f5136bd891968fa4c1
dvpenmetsa <dvpenmetsa@gmail.com>
2021-09-11 20:14:59 -0700

    New Problem

---
+++ src/learn/ds/stack/BasicCaliculator.java
@@ -0,0 +1,111 @@
+package learn.ds.stack;
+
+import java.util.Stack;
+
+/**
+ *Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+ *
+ * Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+ *
+ * Example 1:
+ *
+ * Input: s = "1 + 1"
+ * Output: 2
+ * Example 2:
+ *
+ * Input: s = " 2-1 + 2 "
+ * Output: 3
+ * Example 3:
+ *
+ * Input: s = "(1+(4+5+2)-3)+(6+8)"
+ * Output: 23
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 3 * 105
+ * s consists of digits, '+', '-', '(', ')', and ' '.
+ * s represents a valid expression.
+ * '+' is not used as a unary operation.
+ * '-' could be used as a unary operation but it has to be inside parentheses.
+ * There will be no two consecutive operators in the input.
+ * Every number and running calculation will fit in a signed 32-bit integer.
+ *
+ * https://leetcode.com/problems/basic-calculator/
+ */
+
+public class BasicCaliculator {
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * @param s
+     * @return
+     */
+    public int calculate(String s) {
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int operand = 0;
+        int result = 0; // For the on-going result
+        int sign = 1;  // 1 means positive, -1 means negative
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+
+                // Forming operand, since it could be more than one digit
+                operand = 10 * operand + (int) (ch - '0');
+
+            } else if (ch == '+') {
+
+                // Evaluate the expression to the left,
+                // with result, sign, operand
+                result += sign * operand;
+
+                // Save the recently encountered '+' sign
+                sign = 1;
+
+                // Reset operand
+                operand = 0;
+
+            } else if (ch == '-') {
+
+                result += sign * operand;
+                sign = -1;
+                operand = 0;
+
+            } else if (ch == '(') {
+
+                // Push the result and sign on to the stack, for later
+                // We push the result first, then sign
+                stack.push(result);
+                stack.push(sign);
+
+                // Reset operand and result, as if new evaluation begins for the new sub-expression
+                sign = 1;
+                result = 0;
+
+            } else if (ch == ')') {
+
+                // Evaluate the expression to the left
+                // with result, sign and operand
+                result += sign * operand;
+
+                // ')' marks end of expression within a set of parenthesis
+                // Its result is multiplied with sign on top of stack
+                // as stack.pop() is the sign before the parenthesis
+                result *= stack.pop();
+
+                // Then add to the next operand on the top.
+                // as stack.pop() is the result calculated before this parenthesis
+                // (operand on stack) + (sign on stack * (result from parenthesis))
+                result += stack.pop();
+
+                // Reset the operand
+                operand = 0;
+            }
+        }
+        return result + (sign * operand);
+    }
+}
