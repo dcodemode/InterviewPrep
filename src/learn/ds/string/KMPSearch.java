d57751121d5e81673cf83c45da397db4fba7a918
dvpenmetsa <dvpenmetsa@gmail.com>
2021-10-02 17:10:14 -0700

    Added new problem

--- src/learn/ds/string/KMPSearch.java
+++ src/learn/ds/string/KMPSearch.java
@@ -17,18 +17,20 @@ public class KMPSearch {
      */
     public static int[] computeTemporaryArray(char[] pattern){
         int[] lps = new int[pattern.length];
-        int index = 0;
-        for(int i = 1; i < lps.length; ){
-            if(pattern[index] == pattern[i]){
-                lps[i] = index + 1;
-                index++;
+        int i = 0;
+        int j = 1;
+
+        while(j < pattern.length){
+            if(pattern[i] == pattern[j]){
+                lps[j] = i + 1;
                 i++;
+                j++;
             }else{
-                if(index != 0){
-                    index = lps[index-1];
+                if(i != 0){
+                    i = lps[i-1];
                 }else {
-                    lps[i] = 0;
-                    i++;
+                    lps[j] = 0;
+                    j++;
                 }
             }
         }
