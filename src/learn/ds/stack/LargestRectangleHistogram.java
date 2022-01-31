e0099ad368e81ab4e83203bb4b3c75ed6320bf29
dcoder <dvpenmetsa@gmail.com>
2022-01-30 19:46:58 -0800

    Fixes

--- src/learn/ds/stack/LargestRectangleHistogram.java
+++ src/learn/ds/stack/LargestRectangleHistogram.java
@@ -57,7 +57,7 @@ public class LargestRectangleHistogram {
         int maxArea = 0;
 
         for(int i = 0 ; i < heights.length ; i++){
-            while(st.peek() != -1 && st.peek() >= heights[i]){
+            while(st.peek() != -1 && heights[i] <= heights[st.peek()]){
                 int height = heights[st.pop()];
                 int width = i - st.peek() - 1;
                 maxArea = Math.max(maxArea, height * width);
