e369369dc9aa6ad7af409b8e2c83c952382f0a2c
dcoder <dvpenmetsa@gmail.com>
2022-07-27 22:16:23 -0700

    Added problem discription

---
+++ src/learn/algo/design/CacheMisses.java
@@ -0,0 +1,16 @@
+package learn.algo.design;
+
+
+/**
+ * 
+ * You are give an [array1, array2 ..... arrayN] and give k is the size of the cache
+ * 
+ * Caliculate min cache misses
+ * 
+ * Input : [a,a,a,a,b,b,b,b,b,c,c,c,c,a,a,a] k = 2
+ * Output: 0
+ * 
+ */
+public class CacheMisses {
+    
+}
---
+++ src/learn/ds/array/RockPaperScissorsRock.java
@@ -0,0 +1,47 @@
+package learn.ds.array;
+
+
+/**
+ * 
+ * Given input array where array[0] is the games played by player 1 and array[1] games played by player 2
+ * 
+ * if player1 and player two have same symbol then it is tie, no one gets a point
+ * 
+ * if player 1 wins the round he gets one point;
+ * if player 2 wins the round he gets one point;
+ * 
+ * Return score of the player1 and player2
+ * 
+ * Order:
+ * Rock > Paper > Scissior > Rock
+ * 
+ * Input
+ *  {
+ *    {"R", "P" ,"R"},
+ *    {"P", "P" ,"S"}
+ *  }
+ * 
+ * Output
+ *  {
+ *   "player1" : 1,
+ *   "player2" : 1,
+ *  }
+ * 
+ * Follow Up:
+ * If there are ties, the persion winning after tie will score all values of the tie.
+ * 
+ * Input
+ *  {
+ *    {"R", "P", "P", "R", "R"},  
+ *    {"P", "P", "P", "R", "S"}   
+ *  }
+ * 
+ * Output
+ *  {
+ *   "player1" : 1,
+ *   "player2" : 4,
+ *  }
+ */
+public class RockPaperScissorsRock {
+    
+}
