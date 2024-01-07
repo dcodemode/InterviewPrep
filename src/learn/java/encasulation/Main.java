fbd71670d17ba01d53944a3ebcdb376d84ecfb53
dcoder <dvpenmetsa@gmail.com>
2024-01-07 15:41:28 -0800

    Added encapsulation examples

---
+++ src/learn/java/encasulation/Main.java
@@ -0,0 +1,11 @@
+package learn.java.encasulation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player tim = new Player("Tim", 99,"Sword");
+        System.out.println("Initial health is " + tim.healthRemaining());
+
+        tim.loseHealth(100);
+    }
+}
